eef54cd9d5c99c39fccea2e27d6891c3a559a485
leaf corcoran <leafot@gmail.com>
2017-11-23 10:27:04 -0800

    upgrade deps to react 16

--- static/define_libs.js
+++ static/define_libs.js
@@ -6,7 +6,10 @@ define("lib", {
   chartjs: window["react-chartjs"],
   NoSleep: NoSleep
 });
+
 define("react", React);
 define("react-dom", ReactDOM);
 define("react-router", ReactRouter);
 define("react-router-dom", ReactRouterDOM);
+define("prop-types", PropTypes);
+define("react-transition-group", ReactTransitionGroup);
