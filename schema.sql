99dcc5c97a36f0e88a46ebcfbc0fbd80397725cc
leaf corcoran <leafot@gmail.com>
2021-01-07 12:37:15 -0800

    fix schema file not being updated, update it

--- schema.sql
+++ schema.sql
@@ -2,72 +2,70 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.3
--- Dumped by pg_dump version 9.5.3
+-- Dumped from database version 12.5
+-- Dumped by pg_dump version 12.5
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
+
+--
+-- Name: hourly_hits; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.hourly_hits (
+    user_id integer NOT NULL,
+    hour timestamp without time zone DEFAULT date_trunc('hour'::text, timezone('utc'::text, now())) NOT NULL,
+    type smallint NOT NULL,
+    count integer DEFAULT 0 NOT NULL
+);
+
+
+ALTER TABLE public.hourly_hits OWNER TO postgres;
 
 --
 -- Name: lapis_migrations; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE lapis_migrations (
+CREATE TABLE public.lapis_migrations (
     name character varying(255) NOT NULL
 );
 
 
-ALTER TABLE lapis_migrations OWNER TO postgres;
+ALTER TABLE public.lapis_migrations OWNER TO postgres;
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
+-- Name: presets; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE users (
+CREATE TABLE public.presets (
     id integer NOT NULL,
-    username character varying(255) NOT NULL,
-    encrypted_password character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    slug character varying(255) NOT NULL,
-    last_active timestamp without time zone,
-    display_name character varying(255),
+    user_id integer NOT NULL,
+    data jsonb NOT NULL,
     created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    updated_at timestamp without time zone NOT NULL,
+    name character varying(255) NOT NULL
 );
 
 
-ALTER TABLE users OWNER TO postgres;
+ALTER TABLE public.presets OWNER TO postgres;
 
 --
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: presets_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE users_id_seq
+CREATE SEQUENCE public.presets_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -75,67 +73,212 @@ CREATE SEQUENCE users_id_seq
     CACHE 1;
 
 
-ALTER TABLE users_id_seq OWNER TO postgres;
+ALTER TABLE public.presets_id_seq OWNER TO postgres;
+
+--
+-- Name: presets_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.presets_id_seq OWNED BY public.presets.id;
+
+
+--
+-- Name: song_user_time; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.song_user_time (
+    user_id integer NOT NULL,
+    song_id integer NOT NULL,
+    time_spent integer DEFAULT 0 NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.song_user_time OWNER TO postgres;
+
+--
+-- Name: songs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.songs (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    title text NOT NULL,
+    artist text,
+    album text,
+    source text,
+    song text NOT NULL,
+    players integer DEFAULT 0 NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    publish_status smallint DEFAULT 1 NOT NULL,
+    notes_count integer,
+    beats_duration double precision,
+    original_song_id integer,
+    has_autochords boolean DEFAULT false
+);
+
+
+ALTER TABLE public.songs OWNER TO postgres;
+
+--
+-- Name: songs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.songs_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.songs_id_seq OWNER TO postgres;
+
+--
+-- Name: songs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.songs_id_seq OWNED BY public.songs.id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    username character varying(255) NOT NULL,
+    encrypted_password character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    slug character varying(255) NOT NULL,
+    last_active_at timestamp without time zone,
+    display_name character varying(255),
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    admin boolean DEFAULT false NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
 
 --
 -- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE users_id_seq OWNED BY users.id;
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: presets id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+ALTER TABLE ONLY public.presets ALTER COLUMN id SET DEFAULT nextval('public.presets_id_seq'::regclass);
 
 
 --
--- Name: lapis_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: songs id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY lapis_migrations
+ALTER TABLE ONLY public.songs ALTER COLUMN id SET DEFAULT nextval('public.songs_id_seq'::regclass);
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: hourly_hits hourly_hits_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.hourly_hits
+    ADD CONSTRAINT hourly_hits_pkey PRIMARY KEY (user_id, hour, type);
+
+
+--
+-- Name: lapis_migrations lapis_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.lapis_migrations
     ADD CONSTRAINT lapis_migrations_pkey PRIMARY KEY (name);
 
 
 --
--- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: presets presets_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY users
+ALTER TABLE ONLY public.presets
+    ADD CONSTRAINT presets_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: song_user_time song_user_time_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.song_user_time
+    ADD CONSTRAINT song_user_time_pkey PRIMARY KEY (user_id, song_id);
+
+
+--
+-- Name: songs songs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.songs
+    ADD CONSTRAINT songs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: songs_original_song_id_idx; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX songs_original_song_id_idx ON public.songs USING btree (original_song_id) WHERE (original_song_id IS NOT NULL);
 
 
 --
 -- Name: users_lower_email_idx; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE UNIQUE INDEX users_lower_email_idx ON users USING btree (lower((email)::text));
+CREATE UNIQUE INDEX users_lower_email_idx ON public.users USING btree (lower((email)::text));
 
 
 --
 -- Name: users_lower_username_idx; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE UNIQUE INDEX users_lower_username_idx ON users USING btree (lower((username)::text));
+CREATE UNIQUE INDEX users_lower_username_idx ON public.users USING btree (lower((username)::text));
 
 
 --
 -- Name: users_slug_idx; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE UNIQUE INDEX users_slug_idx ON users USING btree (slug);
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
+CREATE UNIQUE INDEX users_slug_idx ON public.users USING btree (slug);
 
 
 --
@@ -146,25 +289,37 @@ GRANT ALL ON SCHEMA public TO PUBLIC;
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.3
--- Dumped by pg_dump version 9.5.3
+-- Dumped from database version 12.5
+-- Dumped by pg_dump version 12.5
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
-SET search_path = public, pg_catalog;
 
 --
 -- Data for Name: lapis_migrations; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY lapis_migrations (name) FROM stdin;
+COPY public.lapis_migrations (name) FROM stdin;
 1468122484
+1468248948
+1471017619
+1472141760
+1487280271
+1502811966
+1520154640
+1528359105
+1528871228
+1530722873
+1531767217
+1531768259
 \.
 
 
