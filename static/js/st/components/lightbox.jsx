5e24e756ff641258797742a106ec1d491ce8b3fe
leaf corcoran <leafot@gmail.com>
2022-10-19 16:24:51 -0700

    implement focus trap on lightboxes

--- static/js/st/components/lightbox.jsx
+++ static/js/st/components/lightbox.jsx
@@ -4,6 +4,8 @@ import * as types from "prop-types"
 
 import {trigger} from "st/events"
 
+let currentFocusTrap = null
+
 export default class Lightbox extends React.Component {
   static className = null
 
@@ -19,10 +21,32 @@ export default class Lightbox extends React.Component {
     }
 
     document.body.addEventListener("keydown", this.closeListener)
+
+    if (!currentFocusTrap) {
+      // detect if focus has left the lightbox so it can be restored
+      this.detectFocusListener = e => {
+        const container = this.containerRef.current
+        if (e.target != container && !container.contains(e.target)) {
+          container.focus()
+        }
+      }
+
+      document.body.addEventListener("focusin", this.detectFocusListener)
+      currentFocusTrap = this
+    }
+
+    this.containerRef.current.focus()
   }
 
   componentWillUnmount() {
     document.body.removeEventListener("keydown", this.closeListener)
+    if (this.detectFocusListener) {
+      document.body.removeEventListener("focusin", this.detectFocusListener)
+    }
+
+    if (currentFocusTrap == this) {
+      currentFocusTrap = null
+    }
   }
 
   renderContent() {
@@ -42,7 +66,13 @@ export default class Lightbox extends React.Component {
   }
 
   render() {
-    return <div className={classNames("lightbox", this.constructor.className, this.props.className)}>
+    return <div
+      tabIndex="-1"
+      role="dialog"
+      aria-modal="true"
+      onClick={this.props.onClick}
+      ref={this.containerRef ||= React.createRef()}
+      className={classNames("lightbox", this.constructor.className, this.props.className)}>
       {this.renderContent()}
     </div>
   }
